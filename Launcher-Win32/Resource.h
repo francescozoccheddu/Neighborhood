31b1cb515d645567bdf86333fa5ee2e5d8979c76
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-07-03 17:49:22 +0200

    Add Win32 error messages

--- Launcher-Win32/Resource.h
+++ Launcher-Win32/Resource.h
@@ -4,7 +4,11 @@
 //
 #define IDS_TITLE                       101
 #define IDI_ICON                        102
+#define IDS_ERRD_TITLE_NOT_LOADED       102
 #define IDI_ICON_SM                     103
+#define IDS_ERRD_CLASS_NOT_REGISTERED   103
+#define IDS_ERRD_WINDOW_NOT_CREATED     104
+#define IDS_ERRD_ERROR_IN_MESSAGE_LOOP  105
 
 // Next default values for new objects
 // 
