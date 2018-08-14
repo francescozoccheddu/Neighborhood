d21e1332aa4f940767fff97f0fc914fac8fd18cd
Francesco Zoccheddu <zoccheddu.fra@gmail.com>
2018-08-14 18:43:19 +0200

    Lighting draft

--- Projects/Game/Resources/Shaders/lighting_pixel.hlsl
+++ Projects/Game/Resources/Shaders/lighting_pixel.hlsl
@@ -4,6 +4,10 @@ struct PSIn
     float2 TexCoord : TEXCOORD;
 };
 
+cbuffer cbPerFrame
+{
+    float3 mLightPosition;
+};
 
 Texture2D gColorMap;
 Texture2D gNormalMap;
@@ -11,5 +15,8 @@ SamplerState gSamplerState;
 
 float4 main(in PSIn _sIn) : SV_TARGET
 {
-    return gColorMap.Sample(gSamplerState, _sIn.TexCoord);
+    float3 normal = normalize(gNormalMap.Sample(gSamplerState, _sIn.TexCoord).xyz);
+    float3 lightDir = normalize(mLightPosition);
+    float light = saturate(dot(normal, lightDir));
+    return gColorMap.Sample(gSamplerState, _sIn.TexCoord) * light;
 }
